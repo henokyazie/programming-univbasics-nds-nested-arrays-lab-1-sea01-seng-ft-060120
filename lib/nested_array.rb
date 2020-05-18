2556d6d001e30606005766e63ff387937d2c706a
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-18 01:04:49 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,7 +49,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
    row_index = 0 
-   column_index = 0 
+   column_index = 
    
    array[1][1]
   
